a1792a9a186058630a5e23e737fb0c2ac271258b
dianprasetyoo <dipra94@gmail.com>
2019-10-07 20:09:19 +0700

    1.make_list done

---
+++ components/Todo.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+export default class List extends Component  {
+constructor()
+{
+  super()
+  this.state ={
+    todo :['work','swim','study','code','run']
+  }
+}
+
+render () {
+return(
+  <View>
+    {this.state.todo.map((item)=><Text style={styles.container}>{item}</Text>)}    
+  </View>
+)}
+
+}
+
+const styles = StyleSheet.create({
+  container : {
+    fontSize : 20,
+    borderBottomWidth : 2
+  }
+});
